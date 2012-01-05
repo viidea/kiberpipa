b25793f9eefadde4baf10b3041abaa25f93112c9
Jure Cuhalev <gandalfar@gmail.com>
2012-01-05 17:45:30 +0100

    support old kiberpipa style screenshots

--- urls.py
+++ urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 	(r'(?i)^media/(?P<txt>[^/]+)/(play.html|'')$','lectures._custom.kiberpipa.views.urlrw'), 
+	(r'(?i)^media/(?P<slug>[^/]+)/image-i.jpg$','vl.views.screenshot'),
 )
