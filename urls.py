54da4e2049cae79c2ccb505c78b4d309138fe008
Jure Cuhalev <jure@viidea.com>
2012-05-21 12:53:17 +0200

    lets make kiberpipa crash less and 404 less

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls.defaults import *
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
 	(r'(?i)^media/(?P<txt>[^/]+)/(play.html|'')$','lectures._custom.kiberpipa.views.urlrw'), 
 	(r'(?i)^media/(?P<slug>[^/]+)/image-i.jpg$','lectures._custom.kiberpipa.views.image_i'),
 	(r'(?i)^media/(?P<slug>[^/]+)/image-s.jpg$','lectures._custom.kiberpipa.views.image_i'),
+	(r'(?i)^media/(?P<slug>[^/]+)/image.jpg$','lectures._custom.kiberpipa.views.image_i'),
+
+    (r'^rss.xml/$', RedirectView.as_view(url='/site/rss/')),
+
 )
--- views.py
+++ views.py
@@ -11,8 +11,9 @@ from lectures.vl.views import urlrw as vl_urlrw
 
 
 def urlrw(request, txt, template_name=None):
-    if Lecture.objects.filter(slug__istartswith=txt[0:50]).exists():
-        lec = Lecture.objects.filter(slug__istartswith=txt[0:50])[0]
+    text = txt[0:50].replace('.', '')
+    if Lecture.objects.filter(slug__istartswith=text).exists():
+        lec = Lecture.objects.filter(slug__istartswith=text)[0]
         return vl_urlrw(request, lec.slug, template_name)
 
     raise Http404
@@ -21,7 +22,12 @@ def urlrw(request, txt, template_name=None):
 IMAGE_I_SIZE = 500, 500
 @cache_control(must_revalidate=False, max_age=86400)
 def image_i(request, slug, part_id=None):
-    lec = get_object_or_404(Lecture,slug__istartswith=slug[0:50])
+    text = slug[0:50].replace('.', '')
+    if Lecture.objects.filter(slug__istartswith=text).count():
+        lec = Lecture.objects.filter(slug__istartswith=text).order_by('id')[0]
+    else:
+        raise Http404
+
 
     if lec.get_screenshot_att():
         image = Image.open(lec.get_screenshot_att().render.mnt)
